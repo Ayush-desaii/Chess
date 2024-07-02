8ec6bdeb57d5f2f8f9883dd785ab8e7bb37fb1b9
Ayush-desaii <110298454+Ayush-desaii@users.noreply.github.com>
2024-07-02 18:49:58 +0530

    alpha beta

--- AiMove.py
+++ AiMove.py
@@ -3,7 +3,7 @@ import random
 pieceScore = {'K':0, 'Q':10, 'R':5, 'B':3, 'N':3, 'P':1}
 CHECKMATE = 1000
 STALEMATE = 0
-DEPTH = 2
+DEPTH = 3
 
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves) - 1)]
@@ -45,7 +45,7 @@ def findBestMoveNega(gs, validMoves):
     global nextMove
     nextMove = None
     #findMoveMinMax(gs, validMoves, DEPTH, gs.whiteToMove)
-    findMoveNegaMax(gs, validMoves, DEPTH, 1 if gs.whiteToMove else -1)
+    findMoveNegaMaxAlphaBeta(gs, validMoves, DEPTH, -CHECKMATE, CHECKMATE, 1 if gs.whiteToMove else -1)
     return nextMove
 
 def findMoveMinMax(gs, validMoves, depth, whiteToMove):
@@ -94,6 +94,28 @@ def findMoveNegaMax(gs, validMoves, depth, turnMultiplier):
             if depth == DEPTH:
                 nextMove = move
         gs.undoMove()
+    return maxScore
+
+def findMoveNegaMaxAlphaBeta(gs, validMoves, depth, alpha, beta, turnMultiplier):
+    global nextMove
+    if depth == 0:
+        return turnMultiplier * scoreBoard(gs)
+    
+    #move ordering
+    maxScore = -CHECKMATE
+    for move in validMoves:
+        gs.makeMove(move)
+        nextMoves = gs.getValidMoves()
+        score = -findMoveNegaMaxAlphaBeta(gs, nextMoves, depth-1, -beta, -alpha, -turnMultiplier)
+        if score > maxScore:
+            maxScore = score
+            if depth == DEPTH:
+                nextMove = move
+        gs.undoMove()
+        if maxScore > alpha: #pruning
+            alpha = maxScore
+        if alpha >= beta:
+            break
     return maxScore
 
 
